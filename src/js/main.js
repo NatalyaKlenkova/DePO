e1ac29cc9ae02e50e800e46bbf3bf71b5bd93613
Natalia Klenkova <nitklenkova@gmail.com>
2023-02-27 00:14:09 +0300

    popups + map

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,10 @@ const body = document.body;
 const html = document.documentElement;
 
 // Отключение / включение скролла на странице
-const bodyScrollControls = {
-  scrollBarWidth: window.innerWidth - body.clientWidth,
+let bodyScrollControls = {
+  scrollBarWidth: (window.innerWidth - body.clientWidth),
 
-  disable() {
+  disable: function () {
     body.style.marginRight = `${this.scrollBarWidth}px`;
     if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
       body.style.marginRight = null;
@@ -13,7 +13,7 @@ const bodyScrollControls = {
     html.classList.add('stop-scroll');
   },
 
-  enable() {
+  enable: function () {
     body.style.marginRight = null;
     html.classList.remove('stop-scroll');
   }
@@ -26,39 +26,35 @@ const overlay = document.querySelector('.header__overlay');
 const menu = document.querySelector('.menu');
 const menuLinks = document.querySelectorAll('.nav__link');
 
-function enableAndDisableBurgerMenu() {
-    burger.addEventListener('click', function () {
-        overlay.classList.add('overlay--active');
-        menu.classList.add('menu--active');
-        bodyScrollControls.disable();
-    });
-    
-    closeBtn.addEventListener('click', function () {
-        overlay.classList.remove('overlay--active');
-        menu.classList.remove('menu--active');
-        bodyScrollControls.enable();
-    });
-    
-    menuLinks.forEach(function (menuLink) {
-        menuLink.onclick = function () {
-            overlay.classList.remove('header__overlay--active');
-            menu.classList.remove('menu--active');
-            bodyScrollControls.enable();
-        }
-    })
-    
-    document.addEventListener( 'click', (e) => {
-        const withinBoundaries1 = e.composedPath().includes(menuLinks);
-        const withinBoundaries2 = e.composedPath().includes(closeBtn);
-        const withinBoundaries3 = e.composedPath().includes(burger);
-     
-        if ( ! withinBoundaries1 && ! withinBoundaries2 && ! withinBoundaries3 ) {
-            overlay.classList.remove('overlay--active');
-            menu.classList.remove('menu--active');
-            bodyScrollControls.enable();
-        }
-    })
+function enableBurger() {
+  overlay.classList.add('overlay--active');
+  menu.classList.add('menu--active');
+  bodyScrollControls.disable();
 }
+
+function disableBurger() {
+  overlay.classList.remove('overlay--active');
+  menu.classList.remove('menu--active');
+  bodyScrollControls.enable();
+}
+
+burger.addEventListener('click', function () {
+  enableBurger();
+});
+
+closeBtn.addEventListener('click', function () {
+  disableBurger();
+});
+
+menuLinks.forEach(function (menuLink) {
+  menuLink.onclick = function () {
+    disableBurger();
+  }
+})
+
+overlay.addEventListener('click', () => {
+  disableBurger();
+})
 
 // Smooth scroll
 
@@ -77,7 +73,7 @@ for (let anchor of anchors) {
   })
 }
 
-// Language selecting form
+// Language selecting
 const currentLanguage = document.querySelector('.header__lang');
 const currentLanguageValue = document.querySelector('.lang__value');
 const currentLanguageIcon = document.querySelector('.lang__icon');
@@ -85,45 +81,63 @@ const arrow = document.querySelector('.lang__arrow');
 const languageForm = document.querySelector('.lang-options');
 const languageItems = document.querySelectorAll('.lang-options__item');
 
+const burgerLanguageItems = document.querySelectorAll('.menu__lang-item');
+
 function selectLanguage() {
-    // При клике на выбранный язык открывается окно для выбора другого языка
-    currentLanguage.onclick = function () {
-        languageForm.classList.toggle('lang-options--active');
-        arrow.classList.toggle('lang__arrow--active');
+  // Выбор языка в верхней панели
+  // При клике на выбранный язык открывается окно для выбора другого языка
+  currentLanguage.onclick = function () {
+    languageForm.classList.toggle('lang-options--active');
+    arrow.classList.toggle('lang__arrow--active');
+  }
+
+  // Функция, которая меняет язык в верхней панели на выбранный
+  function changeLanguage(event) {
+    let newLanguageValue = event.target.textContent;
+      currentLanguageValue.textContent = newLanguageValue;
+
+
+      // let newLanguage = event.target.parentElement.querySelector(`[xlink:href]`);
+      // // let newLanguageIcon = newLanguage
+      // console.log(newLanguage)
+      // currentLanguageIcon.xlinkHref = '#ua';
+  }
+
+  // При клике на какой-либо из вариантов языка меняется иконка и название языка в панели выше
+  languageItems.forEach(languageItem => {
+    languageItem.onclick = function (event) {
+      document.querySelector('.lang-options__item--active').classList.remove('lang-options__item--active');
+      languageItem.classList.add('lang-options__item--active');
+
+      changeLanguage(event);
+
+      languageForm.classList.remove('lang-options--active');
     }
+  })
 
-    // При клике на какой-либо из вариантов языка меняется иконка и название языка в панели выше
-    languageItems.forEach(languageItem => {
-        languageItem.onclick = function (event) {
-            document.querySelector('.lang-options__item--active').classList.remove('lang-options__item--active');
-            languageItem.classList.add('lang-options__item--active');
+  // При клике на свободную область, форма выбора языка скрывается
+  document.addEventListener('click', (e) => {
+    const withinBoundaries1 = e.composedPath().includes(languageItems);
+    const withinBoundaries2 = e.composedPath().includes(currentLanguage);
 
-            let newLanguageValue = event.target.textContent;
-            currentLanguageValue.textContent = newLanguageValue;
-            
+    if (!withinBoundaries1 && !withinBoundaries2) {
+      languageForm.classList.remove('lang-options--active');
+      arrow.classList.remove('lang__arrow--active');
+    }
+  })
 
-            // let newLanguage = event.target.parentElement.querySelector(`[xlink:href]`);
-            // // let newLanguageIcon = newLanguage
-            // console.log(newLanguage)
-            // currentLanguageIcon.xlinkHref = '#ua';
+  // Панель выбора языков в бургер меню
+  burgerLanguageItems.forEach(burgerLanguageItem => {
+    burgerLanguageItem.onclick = function (event) {
+      document.querySelector('.menu__lang-item--active').classList.remove('menu__lang-item--active');
+      burgerLanguageItem.classList.add('menu__lang-item--active');
 
-            languageForm.classList.remove('lang-options--active');
-        }
-    })
+      changeLanguage(event);
 
-    // При клике на свободную область, форма выбора языка скрывается
-    document.addEventListener( 'click', (e) => {
-        const withinBoundaries1 = e.composedPath().includes(languageItems);
-        const withinBoundaries2 = e.composedPath().includes(currentLanguage);
-        // const withinBoundaries3 = e.composedPath().includes(burger);
-     
-        if ( ! withinBoundaries1 && ! withinBoundaries2 ) {
-            languageForm.classList.remove('lang-options--active');
-            arrow.classList.remove('lang__arrow--active');
-        }
-    })
+      disableBurger();
+    }
+  })
 }
-
 selectLanguage();
 
 // Popups
@@ -133,83 +147,127 @@ const heroPopup = document.querySelector('.hero__popup');
 const heroOverlay = document.querySelector('.hero__overlay');
 const heroPopupCloseBtn = document.querySelector('.hero-popup__closebtn');
 
+function disableHeroOverlay() {
+  heroOverlay.classList.remove('overlay--active');
+  heroPopup.classList.remove('popup--active');
+  bodyScrollControls.enable();
+}
+
 heroBtn.onclick = function () {
-    heroOverlay.classList.add('overlay--active');
-    heroPopup.classList.add('popup--active');
-    bodyScrollControls.disable();
+  heroOverlay.classList.add('overlay--active');
+  heroPopup.classList.add('popup--active');
+  bodyScrollControls.disable();
 }
 
 heroPopupCloseBtn.onclick = function () {
-    heroOverlay.classList.remove('overlay--active');
-    heroPopup.classList.remove('popup--active');
-    bodyScrollControls.enable();
+  disableHeroOverlay();
 }
 
+heroOverlay.onclick = function () {
+  disableHeroOverlay();
+}
+
+// Custom select (geo popup form)
+const element = document.querySelector('.form__select');
+const choices = new Choices(element, {
+  searchEnabled: false,
+  placeholder: true,
+  placeholderValue: 'Выберите страну',
+  shouldSort: false,
+  classNames: {
+    selectedState: 'is-selected',
+  }
+});
+
+// Geo popups
+const geoPopupFirst = document.querySelector('.geo__popup-1');
+const geoPopupSecond = document.querySelector('.geo__popup-2');
+const geoOverlay = document.querySelector('.geo__overlay');
+const geoConnectBtn = document.querySelector('.geo__btn-connect');
+const geoSendBtn = document.querySelector('.geo-popup__btn-send');
+const geoOkBtn = document.querySelector('.geo-popup__btn-ok');
+const geoCloseBtns = document.querySelectorAll('.geo-popup__closebtn');
+
+// Переиспользуемая функция, скрывающая попап
+function disableGeoPopup() {
+  geoOverlay.classList.remove('overlay--active');
+
+  geoPopupFirst.style.visibility = 'hidden';
+  geoPopupSecond.style.visibility = 'hidden';
+
+  setTimeout(() => {
+    geoOverlay.style.transform = 'translateX(0)';
+    bodyScrollControls.enable();
+  }, 1000);
+}
+
+// При нажатии на кнопку "Подлкючить мою страну" появляется попап
+geoConnectBtn.onclick = function () {
+  geoOverlay.classList.add('overlay--active');
+  // 'Проскальзываем' на второй экран
+  geoOverlay.style.transform = 'translateX(-33.33%)';
+
+  geoPopupFirst.style.visibility = 'visible';
+  geoPopupSecond.style.visibility = 'visible';
+
+  bodyScrollControls.disable();
+}
+
+// При нажатии на крестик, попап исчезает
+geoCloseBtns.forEach(function (closeBtn) {
+  closeBtn.addEventListener('click', function () {
+    disableGeoPopup();
+  })
+})
+
+// При нажатии на кнопку "Отправить" в первом окне, перемещаемся на третий экран
+geoSendBtn.onclick = function (evt) {
+  evt.preventDefault();
+  geoOverlay.style.transform = 'translateX(-66.66%)';
+}
+
+// При нажатии на кнопку "Понятно" попап скрывается
+geoOkBtn.onclick = function () {
+  disableGeoPopup();
+}
 
 // Accordion
 
 $(function () {
-    $("#accordion").accordion({
-      collapsible: true,
+  $("#accordion").accordion({
+    collapsible: true,
     //   active: false,
-      heightStyle: "content"
-    });
+    heightStyle: "content"
   });
+});
 
 // Map
-
-const maps = [
-  {
-    target: 'ru',
-    mapURL: 'url(../img/map_ru.png)'
-  },
-  {
-    target: 'by',
-    mapURL: 'url(../img/map_by.png)'
-  },
-  {
-    target: 'ua',
-    mapURL: 'url(../img/map_ua.png)'
-  },
-  {
-    target: 'tr',
-    mapURL: 'url(../img/map_tr.png)'
-  },
-  {
-    target: 'kz',
-    mapURL: 'url(../img/map_kz.png)'
-  },
-]
-
 const activeCountryClass = 'countries__item--active';
 const countriesTabs = Array.from(document.querySelectorAll('.countries__item'));
+const maps = document.querySelectorAll('.geo-map__country');
 
 countriesTabs[0].classList.add(activeCountryClass);
 let path = countriesTabs[0].dataset.path;
 
 function addMapBackground(path) {
-  const mapContainer = document.querySelector('.geo__bg');
-
   for (let map of maps) {
-    if (map.target === path) {
-      mapContainer.style.backgroundImage = map.mapURL
+    map.style.opacity = '0.5';
+    if (map.dataset.target === path) {
+      map.style.opacity = '1';
     }
   }
 }
 
-
 countriesTabs.forEach(tab => {
-  tab.onclick = function (event) {
+  tab.onclick = function () {
     path = tab.dataset.path;
 
     for (let tab of countriesTabs) {
       tab.classList.remove(activeCountryClass);
     }
 
-    tab.classList.add(activeCountryClass)
+    tab.classList.add(activeCountryClass);
 
-    addMapBackground(path)
+    addMapBackground(path);
   }
 })
-
-enableAndDisableBurgerMenu();
